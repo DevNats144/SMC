1a5143052fa858f935332dcd07dde0efc44a21d1
Dev Nats <natanielg73@gmail.com>
2026-09-25 08:37:44 +0100

    Adding the react router dom on my existing vite app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './Style.css'
 import WhatsButton from './RComponents/WhatsButton';
 import Navegacao from './RComponents/Navbar';
